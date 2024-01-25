dc7844d1d58b6d87b6eba036b7c28266b21da089
Travis Rogers <153740421+TravisR27@users.noreply.github.com>
2024-01-25 18:16:40 +0000

    Add files via upload

---
+++ Details.py.py
@@ -0,0 +1,6 @@
+name = input ('Please provide your Full Name? ') # Request input from user, store input from user into variable called 'name'
+print('Hello ' + name + '!') # output message + 'name' in a message
+age = input ('Please provide you age? ') # Request input from user, store input from user into variable called 'age'
+num = input ('Please provide Door number? ') # Request input from user, store input from user into variable called 'num'
+street = input ('Please provide street name? ') # Request input from user, store input from user into variable called 'street'
+print('This is ' + name, 'Aged ' + age, 'Who lives at number', num, street,'.') # output message prints this is name and age who lives at number and street name.
---
+++ conversion.py.py
@@ -0,0 +1,20 @@
+#This is a description on how to change a variable to an integer
+
+num1 = 99.33 
+int_num1 = int (num1) 
+print(int_num1) 
+
+#This is a description on how to change a variable to a float
+num2 = 23 
+float_num2 = float (num2) 
+print(float_num2) 
+
+# This is a description on how to change a variable into a string
+num3 = "150" 
+string_num3 = str(num3) 
+print(string_num3) 
+
+# This is a description on how to change a variable into a integer
+string1 = "100"
+int_string1 = int (string1) 
+print(int_string1) 
