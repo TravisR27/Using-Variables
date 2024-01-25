9fe47d3328028f9af5342a126deecb940bfb53f9
Travis Rogers <153740421+TravisR27@users.noreply.github.com>
2024-01-25 18:34:50 +0000

    Add files via upload

---
+++ Hello_World.py.py
@@ -0,0 +1,6 @@
+name = input ('Please provide your Full Name? ') # Request input from user, store input from user into variable called 'name'
+print('Welcome ' + name + '!') # output message + 'name' in a message
+age = input ('Please provide you age? ') # Request input from user, store input from user into variable called 'age'
+print('Thank You ' + name, 'Aged ' + age, 'for providing this information') # output message + 'name' +_ 'age' in a message
+aString = 'Hello World' # store input into variable called 'aString'
+print ('Hello World') # Output print 'aString' varaible hello world
